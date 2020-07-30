9bb48cf5b273a27aef7d53e1f9c9016e89c9e6c5
Burak Doğukan Dağlı <bdogukandagli@gmail.com>
2020-07-30 14:58:53 +0300

    feat: webCrawl implemented

--- agirlikTest.py
+++ agirlikTest.py
@@ -8,6 +8,8 @@ from snowballstemmer import TurkishStemmer
 import numpy as np
 import random
 import json
+import requests
+import bs4
 
 nltk.download('punkt')
 
@@ -47,6 +49,41 @@ def bag_of_words(s, words):
             if w == se:
                 bag[i] = 1
     return np.array(bag)
+
+
+def covid19(country):
+    res = requests.get("https://www.worldometers.info/coronavirus/#countries")
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    index = -1
+    data = soup.select('tr td')
+    for i in range(len(data)):
+        if data[i].text.lower() == country.lower():
+            index = i
+            break
+    gunlukTablo=""
+    for i in range(7):
+        if i == 0:
+            gunlukTablo="\nÜlke adı: " + str(data[i + index].text)
+        elif i == 1:
+            gunlukTablo=gunlukTablo+'\n'+"Toplam vaka: " + str(data[i + index].text)
+        elif i == 2:
+            if data[i + index].text == '':
+                gunlukTablo=gunlukTablo+'\n'+"Yeni vaka: 0"
+            else:
+                gunlukTablo=gunlukTablo+'\n'+"Yeni vaka: " + str(data[i + index].text)
+        elif i == 3:
+            gunlukTablo=gunlukTablo+'\n'+"Toplam ölüm: " + str(data[i + index].text)
+        elif i == 4:
+            if data[i + index].text == '':
+                gunlukTablo=gunlukTablo+'\n'+"Yeni ölüm: 0"
+            else:
+                gunlukTablo=gunlukTablo+'\n'+"Yeni ölüm: " + str(data[i + index].text)
+        elif i == 5:
+            gunlukTablo=gunlukTablo+'\n'+"Toplam iyileşen: " + str(data[i + index].text)
+        elif i == 6:
+            gunlukTablo=gunlukTablo+'\n'+"Yeni iyileşen: " + str(data[i + index].text)
+    return gunlukTablo
+
 
 # Covid19 olma olasılığının hesaplanması
 def covidOlasilik(cevapListesi):
@@ -129,6 +166,8 @@ def chat(message):
     covidRiskDurumu = covidRisk(covidOlasilikDurumu)
     if message.lower() == "kapat":
         return sonuc(covidOlasilikDurumu, covidRiskDurumu)
+    if message.lower() == "tablo":
+        return covid19("Turkey")
     results = model.predict(np.asanyarray([bag_of_words(message, words)]))[0]
     # print(results)
     results_index = np.argmax(results)
